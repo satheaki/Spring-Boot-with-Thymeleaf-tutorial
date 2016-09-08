adc6126b7d8c092b1b8ce43a9eeac50a9eb03f9a
Akshay <akshaysathe4@gmail.com>
2016-09-08 15:18:26 -0400

    adding files

--- src/test/java/com/jvmhub/springboot/SpringBootThymeleafApplicationTests.java
+++ src/test/java/com/jvmhub/springboot/SpringBootThymeleafApplicationTests.java
@@ -1,10 +1,20 @@
 package com.jvmhub.springboot;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootThymeleafApplication.class)
